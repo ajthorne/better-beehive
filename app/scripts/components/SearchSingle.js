3cb0ff914b758d39a9178e406cdc14fad7209305
Amanda Thorne <manda.thorne@gmail.com>
2016-10-23 14:01:13 -0500

    some stylings for search page

--- app/scripts/components/SearchSingle.js
+++ app/scripts/components/SearchSingle.js
@@ -5,11 +5,27 @@ import {Link} from 'react-router';
 
 const SearchSingle = React.createClass({
   render: function () {
+    let buzzImg;
+
+    if (this.props.buzzes > 70) {
+      buzzImg = <img className="buzzImg" src="../../../assets/images/Buzzworthy.png"/>
+    } else if (this.props.buzzes < 50) {
+      buzzImg = <img className="buzzImg" src="../../../assets/images/Sting.png"/>
+    } else {
+      buzzImg = <img className="buzzImg" src="../../../assets/images/Buzz.png"/>
+
+    }
     return (
       <li className="single-review-holder">
-        <Link to={`/reviews/${this.props.id}`}><p className="single-review-name"><span>Name: </span>{this.props.name}</p></Link>
-        <p className="single-review-number"><span>Number: </span>{this.props.number}</p>
-        <p className="single-review-buzzes"><span>Buzzes: </span>{this.props.buzzes}<span>%</span></p>
+        <div className="rating">
+          <p className="single-review-buzzes">{this.props.buzzes}<span>%</span></p>
+          {buzzImg}
+          <Link to={`/reviews/${this.props.id}`}><p className="single-review-name">{this.props.name}</p></Link>
+        </div>
+        <div className="contact">
+          <p className="single-review-number">{this.props.number}</p>
+          <p className="single-review-address">{this.props.address}</p>
+        </div>
       </li>
     )
   }
--- app/scripts/components/homePage/Nav.js
+++ app/scripts/components/homePage/Nav.js
@@ -7,7 +7,7 @@ const Nav = React.createClass({
       <nav>
         <li><Link to="/">Home</Link></li>
         <li><Link to="#">About</Link></li>
-        <li><Link to="#">BuzzPoints</Link></li>
+        <li><a href="https://medium.com/@BetterBeehive/">Buzz Blog</a></li>
         <li><Link to="#">Contact</Link></li>
       </nav>
     )
