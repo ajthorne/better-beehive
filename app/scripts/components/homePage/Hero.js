34db3ec69515a07b78e3f2bf830e9db23d50f52a
Amanda Thorne <manda.thorne@gmail.com>
2016-10-23 00:02:32 -0500

    hero stylings

--- app/scripts/components/homePage/Hero.js
+++ app/scripts/components/homePage/Hero.js
@@ -4,20 +4,24 @@ import {browserHistory, Link} from 'react-router';
 const Hero = React.createClass({
   searchHandler: function (e) {
     e.preventDefault();
+    // console.dir(document.getElementById('business-type').label);
     let businessType = document.querySelector('.search-field:checked').value;
     browserHistory.push(`/search/type=${businessType}`);
   },
   render: function () {
     return (
-      <section className="Hero">
+      <section className="hero">
+        <figure><img src="http://placekitten.com/250/250"/></figure>
         <h1>Better Beehive Project</h1>
-        <figure><Link to="/buzz"><img src="http://placekitten.com/100/100"/></Link></figure>
-        <figure><Link to="/sting"><img src="http://placekitten.com/100/100"/></Link></figure>
+        <div className="buzz-or-sting">
+          <figure><Link to="/buzz"><img src="http://placekitten.com/100/100"/></Link></figure>
+          <figure><Link to="/sting"><img src="http://placekitten.com/100/100"/></Link></figure>
+        </div>
         <form onSubmit={this.searchHandler} className="search-field">
           <input className="search-field" type="checkbox" id="medical" value="medical"/><label>Medical</label>
           <input className="search-field" type="checkbox" id="social" value="social"/><label>Social</label>
           <input className="search-field" type="checkbox" id="school" value="school"/><label>School</label>
-          <input type="submit" value="submit"/>
+          <button>Submit</button>
         </form>
       </section>
     )
--- app/scripts/components/homePage/Nav.js
+++ app/scripts/components/homePage/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Nav = React.createClass({
   render: function() {
     return (
       <nav>
-        <li><a href="/">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">BuzzPoints</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="#">About</Link></li>
+        <li><Link to="#">BuzzPoints</Link></li>
+        <li><Link to="#">Contact</Link></li>
       </nav>
     )
   }
