1b6954210c4821d74c5455a803aac4b4f3d3ea61
Amanda Thorne <manda.thorne@gmail.com>
2016-10-23 11:16:42 -0500

    who section styled

--- app/scripts/components/homePage/About.js
+++ app/scripts/components/homePage/About.js
@@ -6,10 +6,11 @@ const About = React.createClass({
     return (
       <section className="about-section">
         <div className="about-copy">
+          <h1>Who We Are...</h1>
           <p>Founder, Veronica Martinez, by day is a parent liaison for a social services government agency and by day, night and 24/7 is a mother of two, one of whom has special needs. In both professional and personal life Veronica identified the need for our society to recognize and appreciate institutions that purposefully build their business model on the values of Accessibility, Empowerment and Inclusion of the special needs community.</p>
         </div>
         <figure className="about-pic">
-          <img src="http://placekitten.com/400/400"/>
+          <img src="../../../assets/images/bbp-founder.jpeg"/>
         </figure>
       </section>
     )
